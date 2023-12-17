0d9f61fd6415e2e0436900b09432464a147de322
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-12-17 01:00:08 -0600

    impl print_cudastream

--- intrasm_engine/pytorch/csrc/extensions/pybind.cpp
+++ intrasm_engine/pytorch/csrc/extensions/pybind.cpp
@@ -1,8 +1,20 @@
 // From
 // NVIDIA/TransformerEngine/transformer_engine/pytorch/csrc/extensions/pybind.cpp
+#include <cuda_runtime.h>
+
+#include <iostream>
 
 #include "../extensions.h"
+
+static long print_cudastream(long st) {
+  void* st_v = (void*)st;
+  cudaStream_t stream = static_cast<cudaStream_t>(st_v);
+  std::cout << "stream: " << stream << std::endl;
+  return PyLong_AsLong(PyLong_FromVoidPtr(st_v));
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("print_cudastream", &print_cudastream, "print cudaStream_t");
   // Data structures
   py::class_<CUDAExperimentalGraphConstructor<cudaStream_t>>(
       m, "CUDAExperimentalGraphConstructor")
--- test/test_load_intrasm_engine_extensions.py
+++ test/test_load_intrasm_engine_extensions.py
@@ -3,3 +3,12 @@ if __name__ == "__main__":
     import intrasm_engine_extensions as iee
 
     print(iee.CUDAExperimentalGraphConstructor)
+
+    import torch
+
+    stream = torch.cuda.Stream()
+    print("stream addr <python>: ", stream.cuda_stream)
+    print(
+        "stream addr round-trip <python>: ",
+        iee.print_cudastream(stream.cuda_stream),
+    )
