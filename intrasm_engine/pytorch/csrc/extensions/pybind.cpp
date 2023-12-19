0f2972445aee0cfa6e086ba4f9a52592f248a631
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-12-19 17:47:19 -0600

    add print_cudaevent

--- intrasm_engine/pytorch/csrc/extensions/pybind.cpp
+++ intrasm_engine/pytorch/csrc/extensions/pybind.cpp
@@ -13,10 +13,40 @@ static long print_cudastream(long st) {
   return PyLong_AsLong(PyLong_FromVoidPtr(st_v));
 }
 
+static long print_cudaevent(long st) {
+  void* st_v = (void*)st;
+  if (st_v == nullptr) {
+    throw std::runtime_error(
+        "torch.cuda.event is nullptr. Please initialize it first. Notice that "
+        "currently the event is only initialized after record() is called.");
+  }
+  cudaEvent_t event = static_cast<cudaEvent_t>(st_v);
+  std::cout << "event: " << event << std::endl;
+  return PyLong_AsLong(PyLong_FromVoidPtr(st_v));
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("print_cudastream", &print_cudastream, "print cudaStream_t");
+  m.def("print_cudaevent", &print_cudaevent, "print cudaEvent_t");
   // Data structures
   py::class_<CUDAExperimentalGraphConstructor<cudaStream_t>>(
       m, "CUDAExperimentalGraphConstructor")
       .def(py::init<>());
+  //  .def("register_stream",
+  //       &CUDAExperimentalGraphConstructor<cudaStream_t>::registerStream)
+  //  .def("register_stream_last_activity",
+  //       &CUDAExperimentalGraphConstructor<
+  //           cudaStream_t>::registeStreamLastActivity)
+  //  .def("add_event_record_node",
+  //       &CUDAExperimentalGraphConstructor<cudaStream_t>::addEventRecordNode)
+  //  .def("add_stream_wait_event_node",
+  //       &CUDAExperimentalGraphConstructor<
+  //           cudaStream_t>::addStreamWaitEventNode)
+  //  .def("join", &CUDAExperimentalGraphConstructor<cudaStream_t>::join)
+  //  .def("notify_before_invoking_library_call",
+  //       &CUDAExperimentalGraphConstructor<
+  //           cudaStream_t>::notifyBeforeInvokingLibraryCall)
+  //  .def("notify_after_invoking_library_call",
+  //       &CUDAExperimentalGraphConstructor<
+  //           cudaStream_t>::notifyAfterInvokingLibraryCall);
 }
--- test/test_load_intrasm_engine_extensions.py
+++ test/test_load_intrasm_engine_extensions.py
@@ -1,15 +1,24 @@
 if __name__ == "__main__":
     import intrasm_engine
-    import intrasm_engine_extensions as iee
+    import intrasm_engine_extensions as iee  # Loading iee along without intrasm_engine will trigger libc10.so not found error
 
     print(iee.CUDAExperimentalGraphConstructor)
 
     import torch
 
     stream = torch.cuda.Stream()
-    print("stream addr <python>: ", stream.cuda_stream)
+    print("stream addr <python>: ", hex(stream.cuda_stream))
     # stream.cuda_stream gets the value of cudaStream_t (which is a pointer) according to THCPStream_get_cuda_stream in pytorch/torch/csrc/cuda/Stream.cpp
     print(
         "stream addr round-trip <python>: ",
-        iee.print_cudastream(stream.cuda_stream),
+        hex(iee.print_cudastream(stream.cuda_stream)),
+    )
+
+    event = torch.cuda.Event()
+    event.record()
+    print("event addr <python>: ", hex(event.cuda_event))
+    # event.cuda_event gets the value of cudaEvent_t (which is a pointer) according to THCPEvent_get_cuda_event in pytorch/torch/csrc/cuda/Event.cpp
+    print(
+        "event addr round-trip <python>: ",
+        hex(iee.print_cudaevent(event.cuda_event)),
     )
