20afce2c4cfe8298794d5cff1820e9c7d628d2d1
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-12-19 18:00:31 -0600

    impl CUDAGraphConstructor wrapper

--- intrasm_engine/pytorch/csrc/extensions/pybind.cpp
+++ intrasm_engine/pytorch/csrc/extensions/pybind.cpp
@@ -13,6 +13,15 @@ static long print_cudastream(long st) {
   return PyLong_AsLong(PyLong_FromVoidPtr(st_v));
 }
 
+static std::vector<long> print_cudastreams(std::vector<long> sts) {
+  std::vector<long> ret;
+  std::cout << "streams: " << std::endl;
+  for (auto st : sts) {
+    ret.push_back(print_cudastream(st));
+  }
+  return ret;
+}
+
 static long print_cudaevent(long st) {
   void* st_v = (void*)st;
   if (st_v == nullptr) {
@@ -25,28 +34,74 @@ static long print_cudaevent(long st) {
   return PyLong_AsLong(PyLong_FromVoidPtr(st_v));
 }
 
+void PyWrapper_registerStream(
+    CUDAExperimentalGraphConstructor<cudaStream_t>& graph, long stream) {
+  void* stream_v = (void*)stream;
+  cudaStream_t stream_t = static_cast<cudaStream_t>(stream_v);
+  graph.registerStream(stream_t);
+}
+
+void PyWrapper_addEventRecordNode(
+    CUDAExperimentalGraphConstructor<cudaStream_t>& graph, long event,
+    long stream) {
+  void* event_v = (void*)event;
+  cudaEvent_t event_t = static_cast<cudaEvent_t>(event_v);
+  void* stream_v = (void*)stream;
+  cudaStream_t stream_t = static_cast<cudaStream_t>(stream_v);
+  graph.addEventRecordNode(event_t, stream_t);
+}
+
+void PyWrapper_addStreamWaitEventNode(
+    CUDAExperimentalGraphConstructor<cudaStream_t>& graph, long stream,
+    long event) {
+  void* stream_v = (void*)stream;
+  cudaStream_t stream_t = static_cast<cudaStream_t>(stream_v);
+  void* event_v = (void*)event;
+  cudaEvent_t event_t = static_cast<cudaEvent_t>(event_v);
+  graph.addStreamWaitEventNode(stream_t, event_t);
+}
+
+void PyWrapper_join(CUDAExperimentalGraphConstructor<cudaStream_t>& graph,
+                    std::vector<long> streams, long dst_stream) {
+  std::vector<cudaStream_t> streams_t;
+  for (auto stream : streams) {
+    void* stream_v = (void*)stream;
+    cudaStream_t stream_t = static_cast<cudaStream_t>(stream_v);
+    streams_t.push_back(stream_t);
+  }
+  void* dst_stream_v = (void*)dst_stream;
+  cudaStream_t dst_stream_t = static_cast<cudaStream_t>(dst_stream_v);
+  graph.join(streams_t, dst_stream_t);
+}
+
+void PyWrapper_notifyBeforeInvokingLibraryCall(
+    CUDAExperimentalGraphConstructor<cudaStream_t>& graph, long stream) {
+  void* stream_v = (void*)stream;
+  cudaStream_t stream_t = static_cast<cudaStream_t>(stream_v);
+  graph.notifyBeforeInvokingLibraryCall(stream_t);
+}
+
+void PyWrapper_notifyAfterInvokingLibraryCall(
+    CUDAExperimentalGraphConstructor<cudaStream_t>& graph, long stream) {
+  void* stream_v = (void*)stream;
+  cudaStream_t stream_t = static_cast<cudaStream_t>(stream_v);
+  graph.notifyAfterInvokingLibraryCall(stream_t);
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("print_cudastream", &print_cudastream, "print cudaStream_t");
+  m.def("print_cudastream", &print_cudastreams, "print cudaStream_t");
   m.def("print_cudaevent", &print_cudaevent, "print cudaEvent_t");
   // Data structures
   py::class_<CUDAExperimentalGraphConstructor<cudaStream_t>>(
       m, "CUDAExperimentalGraphConstructor")
-      .def(py::init<>());
-  //  .def("register_stream",
-  //       &CUDAExperimentalGraphConstructor<cudaStream_t>::registerStream)
-  //  .def("register_stream_last_activity",
-  //       &CUDAExperimentalGraphConstructor<
-  //           cudaStream_t>::registeStreamLastActivity)
-  //  .def("add_event_record_node",
-  //       &CUDAExperimentalGraphConstructor<cudaStream_t>::addEventRecordNode)
-  //  .def("add_stream_wait_event_node",
-  //       &CUDAExperimentalGraphConstructor<
-  //           cudaStream_t>::addStreamWaitEventNode)
-  //  .def("join", &CUDAExperimentalGraphConstructor<cudaStream_t>::join)
-  //  .def("notify_before_invoking_library_call",
-  //       &CUDAExperimentalGraphConstructor<
-  //           cudaStream_t>::notifyBeforeInvokingLibraryCall)
-  //  .def("notify_after_invoking_library_call",
-  //       &CUDAExperimentalGraphConstructor<
-  //           cudaStream_t>::notifyAfterInvokingLibraryCall);
+      .def(py::init<>())
+      .def("register_stream", &PyWrapper_registerStream)
+      .def("add_event_record_node", &PyWrapper_addEventRecordNode)
+      .def("add_stream_wait_event_node", &PyWrapper_addStreamWaitEventNode)
+      .def("join", &PyWrapper_join)
+      .def("notify_before_invoking_library_call",
+           &PyWrapper_notifyBeforeInvokingLibraryCall)
+      .def("notify_after_invoking_library_call",
+           &PyWrapper_notifyAfterInvokingLibraryCall);
 }
--- test/test_load_intrasm_engine_extensions.py
+++ test/test_load_intrasm_engine_extensions.py
@@ -2,23 +2,46 @@ if __name__ == "__main__":
     import intrasm_engine
     import intrasm_engine_extensions as iee  # Loading iee along without intrasm_engine will trigger libc10.so not found error
 
-    print(iee.CUDAExperimentalGraphConstructor)
+    def test_load_class():
+        print(iee.CUDAExperimentalGraphConstructor)
 
     import torch
 
-    stream = torch.cuda.Stream()
-    print("stream addr <python>: ", hex(stream.cuda_stream))
-    # stream.cuda_stream gets the value of cudaStream_t (which is a pointer) according to THCPStream_get_cuda_stream in pytorch/torch/csrc/cuda/Stream.cpp
-    print(
-        "stream addr round-trip <python>: ",
-        hex(iee.print_cudastream(stream.cuda_stream)),
-    )
+    def test_print_stream():
+        stream = torch.cuda.Stream()
+        print("stream addr <python>: ", hex(stream.cuda_stream))
+        # stream.cuda_stream gets the value of cudaStream_t (which is a pointer) according to THCPStream_get_cuda_stream in pytorch/torch/csrc/cuda/Stream.cpp
+        print(
+            "stream addr round-trip <python>: ",
+            hex(iee.print_cudastream(stream.cuda_stream)),
+        )
 
-    event = torch.cuda.Event()
-    event.record()
-    print("event addr <python>: ", hex(event.cuda_event))
-    # event.cuda_event gets the value of cudaEvent_t (which is a pointer) according to THCPEvent_get_cuda_event in pytorch/torch/csrc/cuda/Event.cpp
-    print(
-        "event addr round-trip <python>: ",
-        hex(iee.print_cudaevent(event.cuda_event)),
-    )
+    def test_print_streams():
+        streams = [torch.cuda.Stream() for _ in range(10)]
+        print(
+            "streams addr <python>: ",
+            list(map(lambda s: hex(s.cuda_stream), streams)),
+        )
+        print(
+            "streams addr round-trip <python>: ",
+            list(
+                map(
+                    lambda s: hex(iee.print_cudastream(s.cuda_stream)), streams
+                )
+            ),
+        )
+
+    def test_print_event():
+        event = torch.cuda.Event()
+        event.record()
+        print("event addr <python>: ", hex(event.cuda_event))
+        # event.cuda_event gets the value of cudaEvent_t (which is a pointer) according to THCPEvent_get_cuda_event in pytorch/torch/csrc/cuda/Event.cpp
+        print(
+            "event addr round-trip <python>: ",
+            hex(iee.print_cudaevent(event.cuda_event)),
+        )
+
+    test_load_class()
+    test_print_stream()
+    test_print_streams()
+    test_print_event()
