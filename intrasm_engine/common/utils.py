f6bcad95bcbae746053d4ecb4aeae61599291c27
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-12-23 12:21:46 -0600

    add sputnik symbol table

--- intrasm_engine/common/utils.py
+++ intrasm_engine/common/utils.py
@@ -24,21 +24,20 @@ def demangle_cuda_function_name(func_name: str) -> str:
 
 
 if __name__ == "__main__":
-    print(
-        demangle_cuda_function_name(
-            "_ZN3cub18DeviceReduceKernelINS_18Device"
-            "ReducePolicyIN6thrust5tupleIblNS2_9null"
-            "_typeES4_S4_S4_S4_S4_S4_S4_EElNS2_8cuda"
-            "_cub9__find_if7functorIS5"
-            "_EEE9Policy600ENS2_12zip_iteratorINS3"
-            "_INS6_26transform_input_iterator_tIbNS6"
-            "_35transform_pair_of_input_iterators"
-            "_tIbNS2_6detail15normal_iteratorINS2"
-            "_10device_ptrIKfEEEESK_NS2_8equal"
-            "_toIfEEEENSF_12unary_negateINS6"
-            "_8identityEEEEENS6_19counting_iterator"
-            "_tIlEES4_S4_S4_S4_S4_S4_S4_S4_EEEElS9"
-            "_S5_EEvT0_PT3_T1_NS_13GridEvenShareISZ"
-            "_EET2_"
+    import os
+
+    # Read the symbol table .txt at the same directory as this script
+    with open(
+        os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "sputnik_cuda_spmm_symbol_table.txt",
         )
-    )
+    ) as fd:
+        for line in fd:
+            line = line.strip()
+            if line.startswith("STT_FUNC"):
+                mangled_func_name = line.split(" ")[-1]
+                print(
+                    mangled_func_name,
+                    demangle_cuda_function_name(mangled_func_name),
+                )
