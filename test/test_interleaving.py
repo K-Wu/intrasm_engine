f68a89b802bceab15ff88f0167d30d0335ee8447
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2024-01-19 23:09:52 -0600

    upd test_interleaving.py

--- test/test_interleaving.py
+++ test/test_interleaving.py
@@ -16,13 +16,18 @@ def gemm_tflops(m, n, k, msec):
 
 
 def test_triton_simt_and_torch_tensorop_interleave(
-    mt=1280, nt=1024, kt=1024, m=1280, n=1024, k=256
+    mt=1280, nt=1024, kt=1024, m=1280, n=1024, k=256, num_nodes_repetition=8
 ):
-    def test_non_interleaving(a, b, matmul_func):
+    def test_non_interleaving(
+        As: list[torch.Tensor], Bs: list[torch.Tensor], matmul_func
+    ):
+        Cs = []
         constructor = TorchCUDAGraphConstructor()
-        constructor.capture_library_call_begin()
-        c = matmul_func(a, b)
-        constructor.capture_library_call_end()
+        for idx in range(num_nodes_repetition):
+            constructor.capture_library_call_begin()
+            c = matmul_func(As[idx], Bs[idx])
+            constructor.capture_library_call_end()
+            Cs.append(c)
         constructor.instantiate_graph_exec()
         start_event = torch.cuda.Event(enable_timing=True)
         end_event = torch.cuda.Event(enable_timing=True)
@@ -64,14 +69,28 @@ def test_triton_simt_and_torch_tensorop_interleave(
             + gemm_tflops(mt, nt, kt, start_event.elapsed_time(end_event)),
         )
 
+    As = [
+        torch.randn(mt, kt, device="cuda", dtype=torch.float16)
+        for _ in range(num_nodes_repetition)
+    ]
+    Bs = [
+        torch.randn(kt, nt, device="cuda", dtype=torch.float16)
+        for _ in range(num_nodes_repetition)
+    ]
     tensor_core_time = test_non_interleaving(  # Tensor core
-        torch.randn(mt, kt, device="cuda", dtype=torch.float16),
-        torch.randn(kt, nt, device="cuda", dtype=torch.float16),
+        As,
+        Bs,
         torch.matmul,  # FIXME: This cannot be the first torch.matmul invoked; otherwise cublas is not initialized. The current workaround is to print(torch.cuda.current_blas_handle()) before calling this function.
     )
+    As = [
+        torch.randn(m, k, device="cuda") for _ in range(num_nodes_repetition)
+    ]
+    Bs = [
+        torch.randn(k, n, device="cuda") for _ in range(num_nodes_repetition)
+    ]
     simt_time = test_non_interleaving(  # SIMT
-        torch.randn(m, k, device="cuda"),
-        torch.randn(k, n, device="cuda"),
+        As,
+        Bs,
         triton_utils.run_matmul,
     )
     print(
@@ -82,10 +101,13 @@ def test_triton_simt_and_torch_tensorop_interleave(
     )
     print(
         "TFLOPs",
-        gemm_tflops(mt, nt, kt, tensor_core_time),
-        gemm_tflops(m, n, k, simt_time),
-        gemm_tflops(m, n, k, tensor_core_time + simt_time)
-        + gemm_tflops(mt, nt, kt, tensor_core_time + simt_time),
+        num_nodes_repetition * gemm_tflops(mt, nt, kt, tensor_core_time),
+        num_nodes_repetition * gemm_tflops(m, n, k, simt_time),
+        num_nodes_repetition
+        * (
+            gemm_tflops(m, n, k, tensor_core_time + simt_time)
+            + gemm_tflops(mt, nt, kt, tensor_core_time + simt_time)
+        ),
     )
     test_interleaving()
 
@@ -101,83 +123,105 @@ def test_cutlass_simt_and_tensorop_interleave(
     num_nodes_repetition=8,
 ):
     def test_canonical_procedure(do_tensor_core: bool, do_simt: bool):
+        As = []
+        Bs = []
+        Cs = []
+        plan_tcs = []
+        arguments_tcs = []
+        A2s = []
+        B2s = []
+        C2s = []
+        plan_simts = []
+        arguments_simts = []
         constructor = TorchCUDAGraphConstructor()
         if do_tensor_core and do_simt:
             constructor.register_new_stream()
         if do_tensor_core:
-            a = torch.randn(mt, kt, device="cuda", dtype=torch.float16)
-            b = torch.randn(kt, nt, device="cuda", dtype=torch.float16)
-            c = torch.randn(mt, nt, device="cuda", dtype=torch.float16)
-            plan_tc = cutlass.op.Gemm(
-                element=torch.float16,
-                layout=cutlass.LayoutType.RowMajor,
-                element_C=cutlass.DataType.void,
-                element_accumulator=cutlass.DataType.f16,
-            )
-            # for td in plan_tc.tile_descriptions():
-            #     print(td)
-            # plan_tc.tile_description = {
-            #     "threadblock_shape": [128, 256, 32],
-            #     "warp_count": [2, 4, 1],
-            #     "stages": 3,
-            # }
-            plan_tc.tile_description = {
-                "threadblock_shape": [64, 128, 32],
-                "warp_count": [2, 2, 1],
-                "stages": 3,
-            }
-            arguments_tc = cutlass_utils.prepare_GemmArguments(
-                plan_tc,
-                a,
-                b,
-                None,
-                c,
-                print_module=False,
-                stream=cuda.CUstream(
-                    init_value=torch.cuda.current_stream().cuda_stream
-                ),
-            )
+            for _ in range(num_nodes_repetition):
+                a = torch.randn(mt, kt, device="cuda", dtype=torch.float16)
+                b = torch.randn(kt, nt, device="cuda", dtype=torch.float16)
+                c = torch.randn(mt, nt, device="cuda", dtype=torch.float16)
+                plan_tc = cutlass.op.Gemm(
+                    element=torch.float16,
+                    layout=cutlass.LayoutType.RowMajor,
+                    element_C=cutlass.DataType.void,
+                    element_accumulator=cutlass.DataType.f16,
+                )
+                # for td in plan_tc.tile_descriptions():
+                #     print(td)
+                # plan_tc.tile_description = {
+                #     "threadblock_shape": [128, 256, 32],
+                #     "warp_count": [2, 4, 1],
+                #     "stages": 3,
+                # }
+                plan_tc.tile_description = {
+                    "threadblock_shape": [64, 128, 32],
+                    "warp_count": [2, 2, 1],
+                    "stages": 3,
+                }
+                arguments_tc = cutlass_utils.prepare_GemmArguments(
+                    plan_tc,
+                    a,
+                    b,
+                    None,
+                    c,
+                    print_module=False,
+                    stream=cuda.CUstream(
+                        init_value=torch.cuda.current_stream().cuda_stream
+                    ),
+                )
+                As.append(a)
+                Bs.append(b)
+                Cs.append(c)
+                plan_tcs.append(plan_tc)
+                arguments_tcs.append(arguments_tc)
 
         if do_simt:
-            a2 = torch.randn(m, k, device="cuda")
-            b2 = torch.randn(k, n, device="cuda")
-            c2 = torch.randn(m, n, device="cuda")
-            plan_simt = cutlass.op.Gemm(
-                element=torch.float32,
-                layout=cutlass.LayoutType.RowMajor,
-                element_C=cutlass.DataType.void,
-                element_accumulator=cutlass.DataType.f32,
-            )
-            plan_simt.opclass = cutlass.OpcodeClass.Simt
-            # for td in plan_simt.tile_descriptions():
-            #     print(td)
-            plan_simt.tile_description = {
-                "threadblock_shape": [128, 64, 8],
-                "warp_count": [2, 2, 1],
-                "stages": 5,
-            }
-            if do_tensor_core:
-                stream = constructor.registeredStreams[
-                    1
-                ].torch_stream.stream.cuda_stream
-            else:
-                stream = cuda.CUstream(
-                    init_value=torch.cuda.current_stream().cuda_stream
+            for _ in range(num_nodes_repetition):
+                a2 = torch.randn(m, k, device="cuda")
+                b2 = torch.randn(k, n, device="cuda")
+                c2 = torch.randn(m, n, device="cuda")
+                plan_simt = cutlass.op.Gemm(
+                    element=torch.float32,
+                    layout=cutlass.LayoutType.RowMajor,
+                    element_C=cutlass.DataType.void,
+                    element_accumulator=cutlass.DataType.f32,
                 )
-            arguments_simt = cutlass_utils.prepare_GemmArguments(
-                plan_simt,
-                a2,
-                b2,
-                None,
-                c2,
-                print_module=False,
-                stream=stream,
-            )
+                plan_simt.opclass = cutlass.OpcodeClass.Simt
+                # for td in plan_simt.tile_descriptions():
+                #     print(td)
+                plan_simt.tile_description = {
+                    "threadblock_shape": [128, 64, 8],
+                    "warp_count": [2, 2, 1],
+                    "stages": 5,
+                }
+                if do_tensor_core:
+                    stream = constructor.registeredStreams[
+                        1
+                    ].torch_stream.stream.cuda_stream
+                else:
+                    stream = cuda.CUstream(
+                        init_value=torch.cuda.current_stream().cuda_stream
+                    )
+                arguments_simt = cutlass_utils.prepare_GemmArguments(
+                    plan_simt,
+                    a2,
+                    b2,
+                    None,
+                    c2,
+                    print_module=False,
+                    stream=stream,
+                )
+                A2s.append(a2)
+                B2s.append(b2)
+                C2s.append(c2)
+                plan_simts.append(plan_simt)
+                arguments_simts.append(arguments_simt)
 
         if do_tensor_core:
-            for _ in range(num_nodes_repetition):
+            for idx in range(num_nodes_repetition):
                 constructor.capture_library_call_begin()
-                plan_tc.operation.run(arguments_tc)  # Tensor core
+                plan_tcs[idx].operation.run(arguments_tcs[idx])  # Tensor core
                 constructor.capture_library_call_end()
         if do_simt:
             if do_tensor_core:
@@ -185,9 +229,9 @@ def test_cutlass_simt_and_tensorop_interleave(
             else:  # null cm
                 cm = contextlib.nullcontext()
             with cm:
-                for _ in range(num_nodes_repetition):
+                for idx in range(num_nodes_repetition):
                     constructor.capture_library_call_begin()
-                    plan_simt.operation.run(arguments_simt)  # SIMT
+                    plan_simts[idx].operation.run(arguments_simts[idx])  # SIMT
                     constructor.capture_library_call_end()
 
         constructor.instantiate_graph_exec()
