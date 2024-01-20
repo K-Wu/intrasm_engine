7df0eabc722cf6c9f3b318885cdaf6f3769048db
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2024-01-20 13:11:29 -0600

    add test cases in test_interleaving.py

--- test/test_interleaving.py
+++ test/test_interleaving.py
@@ -40,19 +40,42 @@ def test_triton_simt_and_torch_tensorop_interleave(
         return start_event.elapsed_time(end_event)
 
     def test_interleaving():
-        a = torch.randn(mt, kt, device="cuda", dtype=torch.float16)
-        b = torch.randn(kt, nt, device="cuda", dtype=torch.float16)
-        a2 = torch.randn(m, k, device="cuda")
-        b2 = torch.randn(k, n, device="cuda")
+        As = []
+        Bs = []
+        Cs = []
+        A2s = []
+        B2s = []
+        C2s = []
+        for _ in range(num_nodes_repetition):
+            a = torch.randn(mt, kt, device="cuda", dtype=torch.float16)
+            b = torch.randn(kt, nt, device="cuda", dtype=torch.float16)
+            c = torch.zeros((mt, nt), device="cuda", dtype=torch.float16)
+            a2 = torch.randn(m, k, device="cuda")
+            b2 = torch.randn(k, n, device="cuda")
+            c2 = torch.zeros((m, n), device="cuda")
+            As.append(a)
+            Bs.append(b)
+            Cs.append(c)
+            A2s.append(a2)
+            B2s.append(b2)
+            C2s.append(c2)
         constructor = TorchCUDAGraphConstructor()
         constructor.register_new_stream()
-        constructor.capture_library_call_begin()
-        c = torch.matmul(a, b)  # Tensor core
-        constructor.capture_library_call_end()
-        with constructor.registeredStreams[1].torch_stream as cm:
+        for idx in range(num_nodes_repetition):
             constructor.capture_library_call_begin()
-            c2 = triton_utils.run_matmul(a2, b2)  # SIMT
+            # torch.matmul returns the output tensor, i.e., c. Specify it as lhs has no effect but to avoid printing.
+            Cs[idx] = torch.matmul(
+                As[idx], Bs[idx], out=Cs[idx]
+            )  # Tensor core
             constructor.capture_library_call_end()
+        with constructor.registeredStreams[1].torch_stream as cm:
+            for idx in range(num_nodes_repetition):
+                constructor.capture_library_call_begin()
+                # run_matmul returns the output tensor, i.e., c2. Specify it as lhs has no effect but explicitly to avoid printing.
+                C2s[idx] = triton_utils.run_matmul(
+                    A2s[idx], B2s[idx], C2s[idx]
+                )  # SIMT
+                constructor.capture_library_call_end()
         constructor.instantiate_graph_exec()
         start_event = torch.cuda.Event(enable_timing=True)
         end_event = torch.cuda.Event(enable_timing=True)
@@ -65,8 +88,11 @@ def test_triton_simt_and_torch_tensorop_interleave(
         print(f"Interleaved Time event: {start_event.elapsed_time(end_event)}")
         print(
             "TFLOPs",
-            gemm_tflops(m, n, k, start_event.elapsed_time(end_event))
-            + gemm_tflops(mt, nt, kt, start_event.elapsed_time(end_event)),
+            num_nodes_repetition
+            * (
+                gemm_tflops(m, n, k, start_event.elapsed_time(end_event))
+                + gemm_tflops(mt, nt, kt, start_event.elapsed_time(end_event))
+            ),
         )
 
     As = [
@@ -292,7 +318,7 @@ def test_triton_simt_and_cutlass_tensorop_interleave(
     kt=768,
     m=1280,
     n=1024,
-    k=128,
+    k=512,
     num_nodes_repetition=8,
 ):
     def test_canonical_procedure(do_tensor_core: bool, do_simt: bool):
@@ -352,10 +378,12 @@ def test_triton_simt_and_cutlass_tensorop_interleave(
 
         if do_simt:
             for _ in range(num_nodes_repetition):
-                a2 = torch.randn(m, k, device="cuda")
-                b2 = torch.randn(k, n, device="cuda")
+                a2 = torch.randn(m, k, device="cuda", dtype=torch.float16)
+                b2 = torch.randn(k, n, device="cuda", dtype=torch.float16)
+                c2 = torch.zeros(m, n, device="cuda", dtype=torch.float16)
                 A2s.append(a2)
                 B2s.append(b2)
+                C2s.append(c2)
 
         if do_tensor_core:
             for idx_rep in range(num_nodes_repetition):
@@ -372,8 +400,10 @@ def test_triton_simt_and_cutlass_tensorop_interleave(
                 for idx_rep in range(num_nodes_repetition):
                     constructor.capture_library_call_begin()
                     # SIMT
-                    c2 = triton_utils.run_matmul(A2s[idx_rep], B2s[idx_rep])
-                    C2s.append(c2)
+                    # run_matmul returns the output tensor, i.e., c2. Specify it as lhs has no effect but explicitly to avoid printing.
+                    C2s[idx_rep] = triton_utils.run_matmul(
+                        A2s[idx_rep], B2s[idx_rep], C2s[idx_rep]
+                    )
                     constructor.capture_library_call_end()
 
         constructor.instantiate_graph_exec()
@@ -427,13 +457,194 @@ def test_triton_simt_and_cutlass_tensorop_interleave(
     )
 
 
-if __name__ == "__main__":
-    print("Triton SIMT in parallel to Torch TensorOp. Repeat with same data")
-    test_triton_simt_and_torch_tensorop_interleave()
-    print("Cutlass SIMT in parallel to TensorOp. Repeat with same data")
-    test_cutlass_simt_and_tensorop_interleave()
-    print(
-        "Triton SIMT in parallel Cutlass to TensorOp. Repeat with differnet"
-        " data"
+def test_cutlass_tensorop_big_and_small_interleave(
+    # mt=2560, nt=1024, kt=512, m=1280, n=512, k=512
+    mb=2560,
+    # nb=2048,
+    nb=1024,
+    kb=768,
+    m=640,
+    n=512,
+    k=3072,
+    num_nodes_repetition=8,
+):
+    def test_canonical_procedure(do_big: bool, do_small: bool):
+        As = []
+        Bs = []
+        Cs = []
+        plan_tcs = []
+        arguments_tcs = []
+        A2s = []
+        B2s = []
+        C2s = []
+        plan_simts = []
+        arguments_simts = []
+        constructor = TorchCUDAGraphConstructor()
+        if do_big and do_small:
+            constructor.register_new_stream()
+        if do_big:
+            for _ in range(num_nodes_repetition):
+                a = torch.randn(mb, kb, device="cuda", dtype=torch.float16)
+                b = torch.randn(kb, nb, device="cuda", dtype=torch.float16)
+                c = torch.randn(mb, nb, device="cuda", dtype=torch.float16)
+                plan_tc = cutlass.op.Gemm(
+                    element=torch.float16,
+                    layout=cutlass.LayoutType.RowMajor,
+                    element_C=cutlass.DataType.void,
+                    element_accumulator=cutlass.DataType.f16,
+                )
+                # for td in plan_tc.tile_descriptions():
+                #     print(td)
+                # plan_tc.tile_description = {
+                #     "threadblock_shape": [128, 256, 32],
+                #     "warp_count": [2, 4, 1],
+                #     "stages": 3,
+                # }
+                plan_tc.tile_description = {
+                    "threadblock_shape": [128, 256, 32],
+                    "warp_count": [2, 4, 1],
+                    "stages": 3,
+                }
+                # plan_tc.tile_description = {
+                #     "threadblock_shape": [64, 128, 32],
+                #     "warp_count": [2, 2, 1],
+                #     "stages": 3,
+                # }
+                arguments_tc = cutlass_utils.prepare_GemmArguments(
+                    plan_tc,
+                    a,
+                    b,
+                    None,
+                    c,
+                    print_module=False,
+                    stream=cuda.CUstream(
+                        init_value=torch.cuda.current_stream().cuda_stream
+                    ),
+                )
+                As.append(a)
+                Bs.append(b)
+                Cs.append(c)
+                plan_tcs.append(plan_tc)
+                arguments_tcs.append(arguments_tc)
+
+        if do_small:
+            for _ in range(num_nodes_repetition):
+                a2 = torch.randn(m, k, device="cuda", dtype=torch.float16)
+                b2 = torch.randn(k, n, device="cuda", dtype=torch.float16)
+                c2 = torch.randn(m, n, device="cuda", dtype=torch.float16)
+                plan_simt = cutlass.op.Gemm(
+                    element=torch.float16,
+                    layout=cutlass.LayoutType.RowMajor,
+                    element_C=cutlass.DataType.void,
+                    element_accumulator=cutlass.DataType.f16,
+                )
+                plan_simt.opclass = cutlass.OpcodeClass.TensorOp
+                # for td in plan_simt.tile_descriptions():
+                #     print(td)
+                plan_simt.tile_description = {
+                    "threadblock_shape": [64, 64, 32],
+                    "warp_count": [2, 2, 1],
+                    "stages": 3,
+                }
+                if do_big:
+                    stream = constructor.registeredStreams[
+                        1
+                    ].torch_stream.stream.cuda_stream
+                else:
+                    stream = cuda.CUstream(
+                        init_value=torch.cuda.current_stream().cuda_stream
+                    )
+                arguments_simt = cutlass_utils.prepare_GemmArguments(
+                    plan_simt,
+                    a2,
+                    b2,
+                    None,
+                    c2,
+                    print_module=False,
+                    stream=stream,
+                )
+                A2s.append(a2)
+                B2s.append(b2)
+                C2s.append(c2)
+                plan_simts.append(plan_simt)
+                arguments_simts.append(arguments_simt)
+
+        if do_big:
+            for idx in range(num_nodes_repetition):
+                constructor.capture_library_call_begin()
+                plan_tcs[idx].operation.run(arguments_tcs[idx])
+                constructor.capture_library_call_end()
+        if do_small:
+            if do_big:
+                cm = constructor.registeredStreams[1].torch_stream
+            else:  # null cm
+                cm = contextlib.nullcontext()
+            with cm:
+                for idx in range(num_nodes_repetition):
+                    constructor.capture_library_call_begin()
+                    plan_simts[idx].operation.run(arguments_simts[idx])
+                    constructor.capture_library_call_end()
+
+        constructor.instantiate_graph_exec()
+        start_event = torch.cuda.Event(enable_timing=True)
+        end_event = torch.cuda.Event(enable_timing=True)
+        torch.cuda.synchronize()
+        start_event.record()
+        constructor.execute_graph()
+        end_event.record()
+        constructor.synchronize()
+        constructor.destroy_graph_exec()
+        return start_event.elapsed_time(end_event)
+
+    tensor_core_time = test_canonical_procedure(  # Tensor core
+        do_small=False, do_big=True
     )
-    test_triton_simt_and_cutlass_tensorop_interleave()
+    simt_time = test_canonical_procedure(do_small=True, do_big=False)  # SIMT
+    print(
+        "Non-interleaved time event: ",
+        tensor_core_time,
+        simt_time,
+        tensor_core_time + simt_time,
+    )
+    print(
+        "TFLOPs",
+        num_nodes_repetition * gemm_tflops(mb, nb, kb, tensor_core_time),
+        num_nodes_repetition * gemm_tflops(m, n, k, simt_time),
+        num_nodes_repetition
+        * (
+            gemm_tflops(m, n, k, tensor_core_time + simt_time)
+            + gemm_tflops(mb, nb, kb, tensor_core_time + simt_time)
+        ),
+    )
+
+    interleaving_time = test_canonical_procedure(do_small=True, do_big=True)
+    print(
+        "Interleaved time event: ",
+        interleaving_time,
+    )
+    print(
+        "TFLOPs",
+        num_nodes_repetition
+        * (
+            gemm_tflops(m, n, k, interleaving_time)
+            + gemm_tflops(mb, nb, kb, interleaving_time)
+        ),
+    )
+
+
+if __name__ == "__main__":
+    # print(
+    #     "Triton SIMT in parallel to Torch TensorOp. Repeat with different data"
+    # )
+    # test_triton_simt_and_torch_tensorop_interleave()
+    # print("Cutlass SIMT in parallel to TensorOp. Repeat with different data")
+    # test_cutlass_simt_and_tensorop_interleave()
+    # print(
+    #     "Triton SIMT in parallel Cutlass to TensorOp. Repeat with different"
+    #     " data"
+    # )
+    # test_triton_simt_and_cutlass_tensorop_interleave()
+    # print(
+    #     "Cutlass TensorOp big in parallel to small. Repeat with different data"
+    # )
+    test_cutlass_tensorop_big_and_small_interleave()
