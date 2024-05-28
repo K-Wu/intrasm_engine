b92df02d4f01b01a86bb1c7d408b72abd395a936
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2024-05-27 21:31:46 -0500

    correct the sputnik and cutlass API calls in interleaved_linear.py

--- intrasm_engine/pytorch/cpp_extensions/layers_and_funcs/interleaved_linear.py
+++ intrasm_engine/pytorch/cpp_extensions/layers_and_funcs/interleaved_linear.py
@@ -23,9 +23,9 @@ this.single_kernel_autograd_functions: list[type[torch.autograd.Function]] = []
 # The forward and backward propagation of spmm/sddmm are following the scheme at https://github.com/facebookresearch/xformers/blob/6600003c2314af88befcec2cd6662957a662981d/xformers/sparse/_csr_ops.py
 # Deducing the derivatives of SpMM/SDDMM: https://arxiv.org/abs/1909.01315
 
+
 # TODO: generalize to batched input, allow stacked input tensor
 class MyInterleavedLinear(MyAutogradFunc):
-
     @staticmethod
     def num_inputs(**kwargs) -> int:
         return 3
@@ -38,120 +38,133 @@ class MyInterleavedLinear(MyAutogradFunc):
     def num_saved_tensors(**ctx_kwargs) -> int:
         return 3
 
-
     @staticmethod
-    def _forward( # single threaded operation
-        constructor,
-        input,
-        weight,
-        **kwargs
+    def _forward(  # single threaded operation
+        constructor, input, weight, **kwargs
     ):
         # constructor = kwargs["constructor"]
-        
-        if (len(constructor.registeredStreams) < 2):
-            constructor.register_new_stream() # perhaps check if there are already 2 streams there
-        
+
+        if len(constructor.registeredStreams) < 2:
+            constructor.register_new_stream()  # perhaps check if there are already 2 streams there
+
         # ctx.backward_constructor = kwargs["backward_constructor"]
         # ctx.num_streams = kwargs["num_streams"]
         constructor_enabled = kwargs["constructor_enabled"]
         # ctx.stream_beg = kwargs["stream_beg"]
-        
+
         # weight is (out_features, in_features), split vertically (axis 1)
         # input is (1, in_features), split horizontally after transpose (axis 0)
         # operation weight * input^T
         w_shape = weight.shape
-        i_shape = input.shape
-        input = input.t()
-        weight_sparse = weight[:, 0 : int(w_shape[1] / 2)]
-        weight_dense = weight[:, int(w_shape[1] / 2) : ]
-        in_sparse = (input[0 : int(i_shape[0] / 2), :]).unsqueeze(0)
-        in_dense = input[int(i_shape[0] / 2) :, :].contiguous()
-        
-        out_dense = torch.randn(w_shape[0], 1, device = "cuda", dtype = torch.float32)
-        out_sparse = torch.randn(w_shape[0], 1, device = "cuda", dtype = torch.float32)
-        out = torch.zeros(w_shape[0], 1, device = "cuda", dtype = torch.float32)
-        
+        # i_shape = input.shape
+        # input_t = input.t()
+        weight_sparse = weight[:, 0 : w_shape[1] // 2]
+        weight_dense = weight[:, w_shape[1] // 2 :].contiguous()
+        # in_sparse = (
+        #     (input[0 : int(i_shape[0] / 2), :]).unsqueeze(0).contiguous()
+        # )
+        # in_dense = input[int(i_shape[0] / 2) :, :].contiguous()
+
+        out_dense = torch.zeros(
+            input.shape[0], w_shape[1] // 2, device="cuda", dtype=torch.float32
+        ).contiguous()
+        # out_sparse = torch.zeros(
+        #     input.shape[0], w_shape[1] // 2, device="cuda", dtype=torch.float32
+        # ).contiguous()
+        # out = torch.zeros(w_shape[0], 1, device="cuda", dtype=torch.float32)
+
         # weight_csr = weight_sparse.to_sparse_csr()
-        weight_csr = SparseCS(weight_sparse, torch.device("cuda"))
+        weight_csr_t = SparseCS(weight_sparse.t(), torch.device("cuda"))
         # input kept as dense
-                
+
         gemm_op = cutlass.op.Gemm(
-            element = torch.float32,
-            layout_A = cutlass.LayoutType.ColumnMajor,
-            layout_B = cutlass.LayoutType.RowMajor,
-            layout_C = cutlass.LayoutType.ColumnMajor,
-            element_C = cutlass.DataType.void,
-            element_accumulator = cutlass.DataType.f32,
+            element=torch.float32,
+            layout=cutlass.LayoutType.RowMajor,
+            element_C=cutlass.DataType.void,
+            element_accumulator=cutlass.DataType.f32,
         )
-        
         gemm_args = cutlass_utils.prepare_GemmArguments(
             gemm_op,
+            input,
             weight_dense,
-            in_dense,
             None,
             out_dense,
-            print_module = False,
-            stream = constructor.registeredStreams[0].torch_stream.stream.cuda_stream
+            print_module=False,
+            stream=constructor.registeredStreams[
+                0
+            ].torch_stream.stream.cuda_stream,
         )
-        
+
         # b = b.contiguous()
         # Set 1 as the "batch size" in sputnik's SpMM, i.e., set batch size as the "m" in sputnik's SpMM.
         # b = b.unsqueeze(0)
         # To use SpMM, set m as kBlockItemsY because each threadIdx.y works on only one row of A.
 
-        if (constructor_enabled):
+        if constructor_enabled:
             with constructor.registeredStreams[0] as compound_stream:
-                
                 constructor.capture_library_call_begin()
                 # run dense gemm
-                out_dense = gemm_op.operation.run(gemm_args)                
+                gemm_op.operation.run(gemm_args)
                 constructor.capture_library_call_end()
-                
+
             with constructor.registeredStreams[1] as compound_stream:
                 constructor.capture_library_call_begin()
-                
-                out_sparse = torch.ops.iex_ops.spmm_sputnik_reuse_weight(
-                    in_sparse,
-                    weight_csr.row_indices,
-                    weight_csr.values.squeeze(),
-                    weight_csr.row_offsets,
-                    weight_csr.column_indices,
-                    w_shape[0]
+
+                out_sparse = (
+                    torch.ops.iex_ops.spmm_sputnik_reuse_weight(
+                        input.t().unsqueeze(0),
+                        weight_csr_t.row_indices,
+                        weight_csr_t.values.flatten(),
+                        weight_csr_t.row_offsets,
+                        weight_csr_t.column_indices,
+                        weight.shape[1] // 2,
+                    )
+                    .squeeze(0)
+                    .t()
                 )
-                
+
                 constructor.capture_library_call_end()
-                
-            constructor.join([constructor.registeredStreams[1]], constructor.registeredStreams[0])
-            
-            # accumulate results, wait for both streams to finish before executing
-            constructor.instantiate_graph_exec() # instantiates a graph execution object after we build the grpah object, CUDA graph API, measure this time, remove it from total
-            constructor.execute_graph() # actual execution of the graph
-            
-            with constructor.registeredStreams[0] as compound_stream:
-                out = out_dense + out_sparse
-                
-        else:
-            out_dense = gemm_op.operation.run(gemm_args)
-            out_sparse = torch.ops.iex_ops.spmm_sputnik_reuse_weight(
-                in_sparse,
-                weight_csr.row_indices,
-                weight_csr.values,
-                weight_csr.row_offsets,
-                weight_csr.column_indices,
-                w_shape[0]
+
+            constructor.join(
+                [constructor.registeredStreams[1]],
+                constructor.registeredStreams[0],
             )
-            out = out_dense + out_sparse
-            
+
+            # accumulate results, wait for both streams to finish before executing
+            constructor.instantiate_graph_exec()  # instantiates a graph execution object after we build the grpah object, CUDA graph API, measure this time, remove it from total
+            constructor.execute_graph()  # actual execution of the graph
+
+            with constructor.registeredStreams[0] as compound_stream:
+                out = torch.cat((out_sparse, out_dense), dim=1)
+
+        else:
+            gemm_op.operation.run(gemm_args)
+            # To use SpMM, we need to transpose the weight, the dense input and the final output. (We are using the WT*XT(row-major) sputnik kernel while the actual input is X(row-major)*W. )
+            out_sparse = (
+                torch.ops.iex_ops.spmm_sputnik_reuse_weight(
+                    input.t().unsqueeze(0),
+                    weight_csr_t.row_indices,
+                    weight_csr_t.values.flatten(),
+                    weight_csr_t.row_offsets,
+                    weight_csr_t.column_indices,
+                    weight.shape[1] // 2,
+                )
+                .squeeze(0)
+                .t()
+            )
+            out = torch.cat((out_sparse, out_dense), dim=1)
+
         # # is this graph already executed? like a one time graph creation
         # constructor.instantiate_graph_exec()
         # constructor.execute_graph()
-        
+
         return out
 
     @staticmethod
     def _backward(ctx, grad):
-        raise NotImplementedError("backward not implemented for interleaved linear yet")
-    
+        raise NotImplementedError(
+            "backward not implemented for interleaved linear yet"
+        )
 
 
 # import sys
@@ -202,12 +215,12 @@ class MyInterleavedLinear(MyAutogradFunc):
 #     ):
 #         ctx.constructor = kwargs["constructor"]
 #         ctx.constructor.register_new_stream() # perhaps check if there are already 2 streams there
-        
+
 #         # ctx.backward_constructor = kwargs["backward_constructor"]
 #         # ctx.num_streams = kwargs["num_streams"]
 #         ctx.constructor_enabled = kwargs["constructor_enabled"]
 #         # ctx.stream_beg = kwargs["stream_beg"]
-        
+
 #         # weight is (out_features, in_features), split vertically (axis 1)
 #         # input is (1, in_features), split horizontally after transpose (axis 0)
 #         # operation weight * input^T
@@ -218,15 +231,15 @@ class MyInterleavedLinear(MyAutogradFunc):
 #         weight_dense = weight[:, int(w_shape[1] / 2) : ]
 #         in_sparse = input[0 : int(i_shape[0] / 2), :]
 #         in_dense = input[input(i_shape[0] / 2) , :].contiguous()
-        
+
 #         out_dense = torch.randn(w_shape[0], 1, device = "cuda", dtype = torch.float32)
 #         out_sparse = torch.randn(w_shape[0], 1, device = "cuda", dtype = torch.float32)
 #         out = torch.randn(w_shape[0], 1, device = "cuda", dtype = torch.float32)
-        
+
 #         # weight_csr = weight_sparse.to_sparse_csr()
 #         weight_csr = SparseCS(weight_sparse, torch.device("cuda"))
 #         # input kept as dense
-                
+
 #         gemm_op = cutlass.op.Gemm(
 #             element = torch.float32,
 #             layout_A = cutlass.LayoutType.ColumnMajor,
@@ -235,7 +248,7 @@ class MyInterleavedLinear(MyAutogradFunc):
 #             element_C = cutlass.DataType.void,
 #             element_accumulator = cutlass.DataType.f32,
 #         )
-        
+
 #         gemm_args = cutlass.utils.prepare_GemmArguments(
 #             gemm_op,
 #             weight_dense,
@@ -247,7 +260,7 @@ class MyInterleavedLinear(MyAutogradFunc):
 #                 init_value=torch.cuda.current_stream().cuda_stream
 #             )
 #         )
-        
+
 #         # b = b.contiguous()
 #         # Set 1 as the "batch size" in sputnik's SpMM, i.e., set batch size as the "m" in sputnik's SpMM.
 #         # b = b.unsqueeze(0)
@@ -255,15 +268,15 @@ class MyInterleavedLinear(MyAutogradFunc):
 
 #         if (ctx.constructor_enabled):
 #             with ctx.constructor.registeredStreams[0] as compound_stream:
-                
+
 #                 ctx.constructor.capture_library_call_begin()
 #                 # run dense gemm
-#                 out_dense = gemm_op.operation.run(gemm_args)                
+#                 out_dense = gemm_op.operation.run(gemm_args)
 #                 ctx.constructor.capture_library_call_end()
-                
+
 #             with ctx.constructor.registeredStreams[1] as compound_stream:
 #                 ctx.constructor.capture_library_call_begin()
-                
+
 #                 out_sparse = torch.ops.iex_ops.spmm_sputnik_reuse_weight(
 #                     in_sparse,
 #                     weight_csr.row_indices,
@@ -272,12 +285,12 @@ class MyInterleavedLinear(MyAutogradFunc):
 #                     weight_csr.column_indices,
 #                     w_shape[0]
 #                 )
-                
+
 #                 ctx.constructor.capture_library_call_end()
-                
+
 #             # accumulate results, wait for both streams to finish before executing
 #             out = out_dense + out_sparse
-                
+
 #         else:
 #             out_dense = gemm_op.operation.run(gemm_args)
 #             out_sparse = torch.ops.iex_ops.spmm_sputnik_reuse_weight(
@@ -289,14 +302,13 @@ class MyInterleavedLinear(MyAutogradFunc):
 #                 w_shape[0]
 #             )
 #             out = out_dense + out_sparse
-            
+
 #         # # is this graph already executed? like a one time graph creation
 #         # ctx.constructor.instantiate_graph_exec()
 #         # ctx.constructor.execute_graph()
-        
+
 #         return out
 
 #     @staticmethod
 #     def _backward(ctx, grad):
 #         raise NotImplementedError("backward not implemented for interleaved linear yet")
-    
--- tests/test_interleaved_linear.py
+++ tests/test_interleaved_linear.py
@@ -18,30 +18,39 @@ from intrasm_engine.common.compound_streams import CompoundStream
 import contextlib
 
 # custom interleaved linear layer class
-from intrasm_engine.pytorch.cpp_extensions.layers_and_funcs.interleaved_linear import MyInterleavedLinear
+from intrasm_engine.pytorch.cpp_extensions.layers_and_funcs.interleaved_linear import (
+    MyInterleavedLinear,
+)
+
 
 # reutrns dictionary of batched weights and inputs tensors
-def generate_tensors (
+def generate_tensors(
     in_features, out_features, repeat_times
 ) -> dict[str, list[torch.Tensor]]:
     weights = [
-        torch.randn(out_features, in_features, device = "cuda", dtype = torch.float32)
-        for _ in range (repeat_times)
+        torch.randn(
+            out_features, in_features, device="cuda", dtype=torch.float32
+        )
+        for _ in range(repeat_times)
     ]
     inputs = [
-        torch.randn(1, in_features, device = "cuda", dtype = torch.float32)
-        for _ in range (repeat_times)
+        torch.randn(1, in_features, device="cuda", dtype=torch.float32)
+        for _ in range(repeat_times)
     ]
-    
-    return {"weights" : weights, "inputs" : inputs}
 
-def test_interleaved_linear (
-    in_features, out_features, repeat_times
-) :
+    return {"weights": weights, "inputs": inputs}
+
+
+def test_interleaved_linear(in_features, out_features, repeat_times):
     # interleaved_linear = MyInterleavedLinear()
-    
-    weights_and_inputs = generate_tensors(in_features, out_features, repeat_times)
-    weights, inputs = weights_and_inputs["weights"], weights_and_inputs["inputs"]
+
+    weights_and_inputs = generate_tensors(
+        in_features, out_features, repeat_times
+    )
+    weights, inputs = (
+        weights_and_inputs["weights"],
+        weights_and_inputs["inputs"],
+    )
     weights_cpu = [w.cpu() for w in weights]
     inputs_cpu = [i.cpu() for i in inputs]
     outputs = []
@@ -53,19 +62,22 @@ def test_interleaved_linear (
 
     # time around this block
     # for i in range(repeat_times):
-    out = MyInterleavedLinear._forward(constructor,
-                                        inputs[0],
-                                        weights[0],
-                                        constructor_enabled = True)
+    out = MyInterleavedLinear._forward(
+        constructor, inputs[0], weights[0], constructor_enabled=True
+    )
     outputs.append(out)
     # stop timer
-    
+
     out_cpu = out.cpu()
-    out_golden = torch.matmul(weights_cpu[0], inputs_cpu[0].t())
+    out_golden = torch.matmul(inputs_cpu[0], weights_cpu[0])
     # print(out_cpu == out_golden)
     print(out_cpu)
     print(out_golden)
-        
+
+
 if __name__ == "__main__":
-    print("testing interleaved linear layer, non batched input, currently repeating ONLY ONCE")
+    print(
+        "testing interleaved linear layer, non batched input, currently"
+        " repeating ONLY ONCE"
+    )
     test_interleaved_linear(128, 128, 8)
