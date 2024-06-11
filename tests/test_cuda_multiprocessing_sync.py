fd281674da5eff6b384cdd1b4e19678e232b58bb
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2024-06-11 10:32:09 -0500

    documentation

--- tests/test_cuda_multiprocessing_sync.py
+++ tests/test_cuda_multiprocessing_sync.py
@@ -1,6 +1,7 @@
 """
 This process creates a new process and synchronize the two processes using a shared CUDA tensor using CUDAMultiprocessingSync instant.
 The process creation scheme is adapted from https://github.com/davidmin7/dgl/blob/c96a8b3e91d0a6cdbb8b103fe84b1374e94053f9/examples/pytorch/graphsage/train_sampling_pytorch_direct.py
+Spawn is used in creating the new process to make CUDA tensors work with multiple processes. See https://stackoverflow.com/questions/50735493/how-to-share-a-list-of-tensors-in-pytorch-multiprocessing.
 Specifically, MPS is used to set the percentage of SMs allocated to each process. The utility of MPS is at https://github.com/davidmin7/dgl/blob/c96a8b3e91d0a6cdbb8b103fe84b1374e94053f9/examples/pytorch/graphsage/utils.py
-Best practices at https://pytorch.org/docs/stable/multiprocessing.html#sharing-cuda-tensors
+PyTorch has the reference counting mechanism support for multi-processing under the hood, but it is still necessary to follow the best practices. See https://github.com/pytorch/pytorch/blob/main/torch/multiprocessing/cuda_multiprocessing.md, and https://pytorch.org/docs/stable/multiprocessing.html#sharing-cuda-tensors
 """
